87f73b01418226e3c91798712ae9e660efb2dc5c
IlyaPC <krotov891@gmail.com>
2023-02-14 13:27:59 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*    Напишите программу, которая выводит случайное число из 
+отрезка [10, 99] и показывает наибольшую цифру числа.  */
+
+// Task 9
+int n = new Random().Next(10, 100);
+
+int left = n / 10;
+int right = n % 10;
+
+int dig = right;
+
+if (left > right)
+    dig = left;
+
+Console.Write($"{n} -> {dig}");
+
